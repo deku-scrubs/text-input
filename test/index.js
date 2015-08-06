ff8c76af16ab2f2754bfd2ce31b38320fff2e9f8
Andrew Shaffer <darawk@gmail.com>
2015-08-06 01:14:41 -0700

    update to deku 5.0

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@
  */
 import assert from 'assert'
 import empty from 'component-empty'
-import {tree, element, render} from 'deku'
+import {tree, render} from 'deku'
+import element from 'virtual-element'
 import TextInput from '..'
 
 /**
